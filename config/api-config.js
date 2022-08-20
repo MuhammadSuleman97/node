1daa44f1e4aadacc3d9207761e5e805cd61907ab
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2022-08-20 15:39:34 +0500

    server file edited

--- config/api-config.js
+++ config/api-config.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-port = 3000;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());  
 
 app.use(cors())
-app.listen(port, ()=> {
-    console.log('listening on port: ', port)
+app.listen(PORT, ()=> {
+    console.log('listening on port: ', PORT)
 })
 
 module.exports = app
