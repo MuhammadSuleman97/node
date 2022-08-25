8360c1095a5253cdfd7d78e8dddd26f70a9ddc08
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2022-08-26 01:29:23 +0500

    restrat server

--- utils/pusher.js
+++ utils/pusher.js
@@ -34,7 +34,6 @@ const questionAttemptEvent = async (email, question_id)  => {
         console.log("Error processing questionAttempted event: " + e.message); 
         return
     }
-    
 }
 
 module.exports = {
