6066b8b8448db2710217ae6697c209da012c1f9e
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2022-08-26 12:12:53 +0500

    Core Added

--- config/api-config.js
+++ config/api-config.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());  
 
-// app.use(cors())
+app.use(cors())
 app.listen(PORT, ()=> {
     console.log('listening on port: ', PORT)
 })
