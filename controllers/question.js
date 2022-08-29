3878848eec8dea272122001f4497f872c22fb64c
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2022-08-29 10:55:50 +0500

    ML model ADded

--- controllers/question.js
+++ controllers/question.js
@@ -79,7 +79,7 @@ exports.submitAnswer = async (req, res) => {
     // Write request here;
     let percentage;
     await axios
-    .post('http://939e-35-236-178-157.ngrok.io/please', {
+    .post('http://939e-35-236-1rok.io/please', {
         ques_id: question_id,
         text: answer
     })
@@ -97,7 +97,7 @@ exports.submitAnswer = async (req, res) => {
     let ans = {
         question_id: question_id,
         answer: answer,
-        score: percentage,
+        score: percentage ? percentage : 0,
         submit_at : new Date()
     }
 
