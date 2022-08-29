1ad7ba196a3ed4af0d2169b26d50a8f77ea4a937
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2022-08-29 10:57:24 +0500

    Implemented

--- controllers/question.js
+++ controllers/question.js
@@ -79,7 +79,7 @@ exports.submitAnswer = async (req, res) => {
     // Write request here;
     let percentage;
     await axios
-    .post('http://939e-35-236-1rok.io/please', {
+    .post('http://939e-35-236-178-157.ngrok.io/please', {
         ques_id: question_id,
         text: answer
     })
